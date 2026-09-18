24842eab29acf465bd082ec7a0e30dad01943e76
luisgrasso <grasso.luis@gmail.com>
2026-09-18 17:15:21 -0300

    qa: remove redundant smoke test

--- tests/qa/smoke.spec.js
+++
@@ -1,5 +0,0 @@
-const { test, expect } = require('@playwright/test');
-test('smoke', async ({ page }) => {
-  await page.goto('/index.html');
-  await expect(page.locator('body')).not.toHaveText('');
-});
